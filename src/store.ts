ae3cae3bc001f143bd8672eefc1d2ab22d3dbca9
Lukas Oppermann <lukas@vea.re>
2018-03-18 19:14:38 +0100

    add annotations

--- src/store.ts
+++ src/store.ts
@@ -2,34 +2,54 @@
 import defaultConfiguration from './defaultConfiguration'
 let store = new Map()
 /**
+ * Stores data & configurations per Sortable
  * @param {Object} config
  */
 export class Store {
   private _config: Map<string, any>|null = null // eslint-disable-line no-undef
 
-  constructor (config: object | null | undefined) {
+  constructor (config?: object) {
     this.config = config
   }
-
-  set config (config: object | null | undefined) {
+  /**
+   * set the configuration of a class instance
+   * @method config
+   * @param  config object of configurations
+   */
+  set config (config?: object): void {
     // combine config with default
     config = Object.assign({}, defaultConfiguration, (typeof config === 'object') ? config : {})
     // add config to map
     this._config = new Map(Object.entries(config))
   }
-
-  get config () {
+  /**
+   * get the configuration map of a class instance
+   * @method config
+   * @return {Map<string, any>}
+   */
+  get config (): Map<string, any> {
     return this._config
   }
-
-  setConfig (key: string, value: any) {
+  /**
+   * set individual configuration of a class instance
+   * @method setConfig
+   * @param  key valid configuration key
+   * @param  value any value
+   * @return void
+   */
+  setConfig (key: string, value: any): void {
     if (!this._config.has(key)) {
       throw new Error(`Trying to set invalid configuration item: ${key}`)
     }
     this._config.set(key, value)
   }
-
-  getConfig (key: string) {
+  /**
+   * set individual configuration of a class instance
+   * @method setConfig
+   * @param  key valid configuration key
+   * @return any configuration value
+   */
+  getConfig (key: string): any {
     if (!this._config.has(key)) {
       throw new Error(`Invalid configuration item requested: ${key}`)
     }
@@ -54,6 +74,7 @@ export class Store {
 }
 /**
  * @param {Element} sortableElement
+ * @param {Object} config - optional, configuration object
  * @returns {Class: Store}
  */
 export default (sortableElement: Element, config?: object) => {
